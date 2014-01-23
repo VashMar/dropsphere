6b6d01e236b40987639e4e1800ea3b030ea94749
nick <pancake@localhost.localdomain>
2014-01-23 12:10:20 -0600

    basic bookmarklet, basic styles

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -1,15 +1,21 @@
 javascript:(function(){
 var d = document.createElement("div");
-d.setAttribute('id', 'dropsphere');
-d.style.width = "400px";
+d.setAttribute("id", "dropsphere");
+d.style.width = "300px";
 d.style.height = "100%";
-d.style.background = "black";
-d.style.color = "white";
+d.style.background = "#eee";
+d.style.color = "#222";
 d.style.position = "fixed";
 d.style.top="0";
-d.style.left="0";
+d.style.right="0";
 
-d.innerHTML = '<iframe id="myFrame" style="width:100%;height:100%; overflow:hidden" src="http://localhost:3500/bookmark">';
+ifrm = document.createElement("iframe");
+ifrm.setAttribute("src", "http://localhost:3500/bookmark");
+ifrm.style.width = "100%";
+ifrm.style.height = "100%";
+ifrm.style.overflow = "hidden";
+ifrm.style.border = "none";
+d.appendChild(ifrm); 
 document.body.appendChild(d);
 
 })();  
---
+++ public/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('#content').slimScroll({
+    	height: '100%'
+    });
+});
