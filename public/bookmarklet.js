6eb5ca80e867d3199bda50b9369f7d34a558257d
nick <pancake@localhost.localdomain>
2014-01-26 12:34:55 -0600

    settings button working with dropdown + modal

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -43,12 +43,13 @@ if(typeof dropsphere === "undefined"){
 	position:absolute;
 	top:0;
 	right:0;
+	margin:2px 6px 0 0;
 	height:30px;
 	width:30px;
-	background:url('http://localhost:3500/close.png') no-repeat;
+	background:url(\"http://localhost:3500/close.png\") no-repeat;
 	}
 	#close:hover{
-	background:url('http://localhost:3500/close_hover.png') no-repeat;
+	background:url(\"http://localhost:3500/close_hover.png\") no-repeat;
 }
 	";
 	document.body.appendChild(css);
