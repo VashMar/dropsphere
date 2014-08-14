2de442cea6eaf4120014810bc466c55eb2037973
VashMar <marada@wisc.edu>
2014-08-14 15:23:02 -0500

    added log output on craWl

--- index.js
+++ index.js
@@ -271,8 +271,9 @@ sessionSockets.on('connection', function (err, socket, session){
 
         request(url, function(err, response, html){
           if (!err && response.statusCode == 200){
+            console.log("URL crawled");
             var $ = cheerio.load(html);
-
+            console.log("the html " + html);
             type = "link";
             title = $("title").text();
             thumbnail = $("meta[property='og:image']").attr('content') ||
