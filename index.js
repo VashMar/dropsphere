263e70042e93faa23b82191b0a76cde0cc318633
VashMar <marada@wisc.edu>
2014-08-28 00:44:38 -0500

    logging textPost data

--- index.js
+++ index.js
@@ -372,7 +372,7 @@ sessionSockets.on('connection', function (err, socket, session){
     // if the posted content is text
     socket.on('textPost', function(data){
       
-      console.log("Posting Data for Text Post..");
+      console.log("Posting Data for Text Post.." + data );
 
       var sphereString = String(data.sphere);               // we need the sphere id in string format for emitting 
       var sphereClients = Object.keys(io.sockets.adapter.rooms[sphereString]);        // get all the user connections in the sphere
