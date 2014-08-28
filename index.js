b762eb048d9df32648692c2d0b4f4ffb8160119b
VashMar <marada@wisc.edu>
2014-08-28 01:29:54 -0500

    json check added for textPost even

--- index.js
+++ index.js
@@ -372,8 +372,23 @@ sessionSockets.on('connection', function (err, socket, session){
     // if the posted content is text
     socket.on('textPost', function(data){
       
+      var json; 
+      try {
+        json = JSON.parse(data);
+      } catch (exception) {
+        console.log("This isn't JSON");
+      }
+
+      if (json) {
+        console.log("this is json");
+      }
+
+
+
       console.log("Posting Data for Text Post.." + data);
       console.log(data[0]);
+
+
 
       var sphereString = String(data.sphere);               // we need the sphere id in string format for emitting 
       var sphereClients = Object.keys(io.sockets.adapter.rooms[sphereString]);        // get all the user connections in the sphere
