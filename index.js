5ace072468508062f4299712a8eac0483deb6bd3
VashMar <marada@wisc.edu>
2014-08-28 16:52:31 -0500

    JSON stringified rooms log to see object

--- index.js
+++ index.js
@@ -386,7 +386,7 @@ sessionSockets.on('connection', function (err, socket, session){
           console.log("Id at sphere key: " + data.sphere);
           var sphereString = String(data.sphere);               // we need the sphere id in string format for emitting
           console.log("sphereString: " + sphereString); 
-          console.log("Rooms: " + io.sockets.adapter.rooms);
+          console.log("Rooms: " + JSON.stringify(io.sockets.adapter.rooms));
           console.log("Rooms at sphereString: " + io.sockets.adapter.rooms[sphereString]);
           var sphereClients = Object.keys(io.sockets.adapter.rooms[sphereString]);        // get all the user connections in the sphere
           var title = data.title;
